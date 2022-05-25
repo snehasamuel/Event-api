f2e5de965e4fbd309f0881ff38bbefd0a84242a8
snehasamuel <snehasamuel297@gmail.com>
2022-05-25 22:28:14 +0530

    api created

---
+++ app.js
@@ -0,0 +1,47 @@
+const Express=require("express")
+const Mongoose=require("mongoose")
+const Bodyparser=require("body-parser")
+
+let app=Express()
+app.use(Bodyparser.urlencoded({extended:true}))
+app.use(Bodyparser.json())
+
+var eventModel=Mongoose.model("events",
+new Mongoose.Schema(
+    {
+        name:String,
+        date:String,
+        venue:String,
+        organiser:String,
+        number:String
+    }
+))
+
+Mongoose.connect("mongodb+srv://snehasam:snehasa4@cluster0.yyrcr.mongodb.net/Eventdb")
+
+app.post("/api/addevent",(req,res)=>{
+var getName=req.body.name 
+var getDate=req.body.date 
+var getVenue=req.body.venue 
+var getOrganiser=req.body.organiser
+var getNumber=req.body.number 
+var data={"name":getName,"date":getDate,"venue":getVenue,"organiser":getOrganiser,"number":getNumber}
+let eventdata=new eventModel(data)
+eventdata.save((error,data)=>{
+    if(error)
+    {
+        res.send({"status":"error","data":error})
+    }
+    else
+    {
+        res.send({"status":"success","data":data})
+    }
+})
+})
+
+app.get("/api/viewevent/",(req,res)=>{
+res.send("Hello")
+})
+app.listen("7000",()=>{
+    console.log("server running")
+})
